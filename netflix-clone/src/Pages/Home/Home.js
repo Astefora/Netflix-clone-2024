495b9152f52ece0c4b74f00d2990db21a2371811
aste-0973 <152900534+Astefora@users.noreply.github.com>
2024-01-27 15:41:39 -0500

    rows

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Header from '../../Component/Header/Header'
 import Footer from '../../Component/Footer/Footer'
 import Banner from '../../Component/Banner/Banner'
+import Row from '../../Component/Rows/Row/Row'
+import Rowlist from '../../Component/Rows/Rowslist/Rowlist'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Banner/>
-      <Footer/>
-      </div>
-  )
+      <Banner />
+      <Rowlist />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home
